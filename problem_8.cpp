bd4c5965300b4db0566b3d3315c0561ccbb10d20
aayeshat <aayesha.tariq@uio.no>
2021-09-12 23:22:37 +0200

    problem 8

--- problem_8.cpp
+++ problem_8.cpp
@@ -4,17 +4,16 @@
 #include <iomanip>
 #include <cmath>
 
-
 //  function: u(x) = 1- (1-exp(-10))x -exp(-10x)
 double u(double x)
 {
-    return 1 - (1 - exp(-10))* x - exp(-10 * x);
+    return 1 - (1 - exp(-10)) * x - exp(-10 * x);
 }
 
 // Exact second derivative: u''(x) = -100exp(-10 x)
 double calc_d2u_exact(double x)
 {
-    return 100 * exp(-10* x);
+    return 100 * exp(-10 * x);
 }
 
 // Approximation of second derivative: (u(x+h) - 2*u(x) + u(x-h)) / h^2
@@ -25,42 +24,56 @@ double calc_d2u_approx(double x, double h)
 
 int main()
 {
-    std::string filename = "problem_8clear_output.txt";
+    std::string filename = "problem_8_output.txt";
     std::ofstream ofile;
     ofile.open(filename);
 
     int width = 18;
     int prec = 10;
-    double x = 2.0;
-
-    // Exact
-    double d2u_exact = calc_d2u_exact(x);
 
     // Range of stepsizes
     double hmin = 1.0e-8;
     double hmax = 1.0;
 
-    std::cout << "#" << std::setw(width - 1) << "stepsize"
-              << std::setw(width) << "d2u_approx"
-              << std::setw(width) << "d2u_exact"
-              << std::endl;
+    int n = 3;
+    double x[n] = {2.0, 3.0, 5.0};
 
-  
-   for (double h = hmin; h <= hmax; h = h * 10.)
-   {
-   
-        double d2u_approx = calc_d2u_approx(x, h);
+    std::cout << "# " << "stepsize";
 
-        std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << h
-                  << std::setw(width) << std::setprecision(prec) << std::scientific << d2u_approx
-                  << std::setw(width) << std::setprecision(prec) << std::scientific << d2u_exact
-                  << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << std::setw(width) << "d" << x[i] << "u_approx"
+                  << std::setw(width) << "d" << x[i] << "u_exact";
+    }
 
-        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << h
-              << std::setw(width) << std::setprecision(prec) << std::scientific << d2u_approx
-              << std::setw(width) << std::setprecision(prec) << std::scientific << d2u_exact
-              << std::endl;
+    std::cout << std::endl;
 
+    double du_exact_array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        du_exact_array[i] = calc_d2u_exact(x[0]);
+    }
+
+    for (double h = hmin; h <= hmax; h = h * 10.)
+    {
+        std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << h;
+        ofile << std::setw(width) << std::setprecision(prec) << std::scientific << h;
+
+        for (int i = 0; i < n; i++)
+        {
+            double du_approx = calc_d2u_approx(x[i], h);
+            double du_exact = du_exact_array[i];
+
+            std::cout << std::setw(width) << std::setprecision(prec) << std::scientific << du_approx
+                      << std::setw(width) << std::setprecision(prec) << std::scientific << du_exact;
+
+            ofile << std::setw(width) << std::setprecision(prec) << std::scientific << du_approx
+                  << std::setw(width) << std::setprecision(prec) << std::scientific << du_exact;
+        }
+
+        std::cout << std::endl;
+        ofile << std::endl;
     }
 
     ofile.close();
--- problem_8_plot.py
+++ problem_8_plot.py
@@ -5,19 +5,25 @@ import matplotlib.pyplot as plt
 # Read data into numpy arrays
 #
 
-h, d2u_approx, d2u_exact = np.loadtxt("problem_8_output.txt", usecols=(0,1,2), unpack=True)
+h, d2u_approx, d2u_exact, d3u_approx, d3u_exact, d4u_approx, d4u_exact = np.loadtxt(
+    "problem_8_output.txt", usecols=(0, 1, 2, 3, 4, 5, 6), unpack=True
+)
 
-# Compute numpy arrays with the absolute error, relative error and log10(relative error)
-abs_err = np.absolute(d2u_approx - d2u_exact)
-rel_err = np.absolute(abs_err / d2u_exact)
 
 log10_h = np.log10(h)
-log10_rel_err = np.log10(rel_err)
 
+d2u_abs_err = np.absolute(d2u_approx - d2u_exact)
+d2u_rel_err = np.absolute(d2u_abs_err / d2u_exact)
+log10_d2u_rel_err = np.log10(d2u_rel_err)
 
-#
-# Make plots
-#
+d3u_abs_err = np.absolute(d3u_approx - d3u_exact)
+d3u_rel_err = np.absolute(d3u_abs_err / d3u_exact)
+log10_d3u_rel_err = np.log10(d3u_rel_err)
+
+d4u_abs_err = np.absolute(d4u_approx - d4u_exact)
+d4u_rel_err = np.absolute(d4u_abs_err / d4u_exact)
+log10_d4u_rel_err = np.log10(d4u_rel_err)
+
 
 # Common title string
 exact_result = d2u_exact[0]
@@ -30,42 +36,35 @@ figheight = figwidth / 1.33333
 
 # Plot absolute error vs h on log-log axes
 plt.figure(figsize=(figwidth, figheight))
-plt.plot(h, abs_err, '--', c="0.8", linewidth=1.5)
-plt.plot(h, abs_err, '.', c="black", markersize=10)
+plt.plot(h, d2u_abs_err, "--", c="0.8", linewidth=1.5)
+plt.plot(h, d2u_abs_err, ".", c="black", markersize=10)
+
+plt.plot(h, d3u_abs_err, "--", c="0.8", linewidth=1.5)
+plt.plot(h, d3u_abs_err, ".", c="black", markersize=10)
+
+plt.plot(h, d4u_abs_err, "--", c="0.8", linewidth=1.5)
+plt.plot(h, d4u_abs_err, ".", c="black", markersize=10)
+
 plt.title("Absolute error " + common_title_string, fontsize=10)
 plt.ylabel("absolute error")
 plt.xlabel("h")
 plt.xscale("log")
 plt.yscale("log")
-plt.savefig("abs_err_vs_h_loglog.pdf")
+plt.savefig("abs_err_vs_h.pdf")
 
-# Plot relative error vs h on log-log axes
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(h, rel_err, '--', c="0.8", linewidth=1.5)
-plt.plot(h, rel_err, '.', c="black", markersize=10)
-plt.title("Relative error " + common_title_string, fontsize=10)
-plt.ylabel("relative error")
-plt.xlabel("h")
-plt.xscale("log")
-plt.yscale("log")
-plt.savefig("rel_err_vs_h_loglog.pdf")
-
-# Plot absolute error vs log10(h)
-plt.figure(figsize=(figwidth, figheight))
-plt.plot(log10_h, abs_err, '--', c="0.8", linewidth=1.5)
-plt.plot(log10_h, abs_err, '.', c="black", markersize=10)
-plt.title("Absolute error " + common_title_string, fontsize=10)
-plt.ylabel("absolute error")
-plt.xlabel("log10(h)")
-plt.savefig("abs_err_vs_log10_h.pdf")
 
 # Plot log10(relative error) vs log10(h)
 plt.figure(figsize=(figwidth, figheight))
-plt.plot(log10_h, log10_rel_err, '--', c="0.8", linewidth=1.5)
-plt.plot(log10_h, log10_rel_err, '.', c="black", markersize=10)
+plt.plot(log10_h, log10_d2u_rel_err, "--", c="0.8", linewidth=1.5)
+plt.plot(log10_h, log10_d2u_rel_err, ".", c="black", markersize=10)
+
+plt.plot(log10_h, log10_d3u_rel_err, "--", c="0.8", linewidth=1.5)
+plt.plot(log10_h, log10_d3u_rel_err, ".", c="black", markersize=10)
+
+plt.plot(log10_h, log10_d4u_rel_err, "--", c="0.8", linewidth=1.5)
+plt.plot(log10_h, log10_d4u_rel_err, ".", c="black", markersize=10)
+
 plt.title("Log10(relative error) " + common_title_string, fontsize=10)
 plt.ylabel("log10(relative error)")
 plt.xlabel("log10(h)")
 plt.savefig("log10_rel_err_vs_log10_h.pdf")
-
-
